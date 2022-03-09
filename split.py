cf38a97f6f9b8bdad6c2f9d7761257e081a207c3
totoadel <mh_882005@yahoo.com>
2022-03-09 06:19:15 +0200

    Update split.py

--- split.py
+++ split.py
@@ -25,18 +25,18 @@ import shutil
 Path("train").mkdir(parents=True, exist_ok=True)
 Path("valid").mkdir(parents=True, exist_ok=True)
 count = 0
-# for filename in glob.glob('images/*.jpg'):
-#     basename_without_ext = os.path.splitext(os.path.basename(filename))[0]
-#     img = cv2.imread(filename)
-#     print(filename)
-#     count = count + 1
-#     if count == 4:
-#         shutil.copyfile('images/' + basename_without_ext + '.txt', 'valid/' + basename_without_ext + '.txt')
-#         cv2.imwrite('valid/' + basename_without_ext + '.jpg', img)
-#         count = 0
-#     else:
-#         shutil.copyfile('images/' + basename_without_ext + '.txt', 'train/' + basename_without_ext + '.txt')
-#         cv2.imwrite('train/' + basename_without_ext + '.jpg', img)
+for filename in glob.glob('images/*.jpg'):
+     basename_without_ext = os.path.splitext(os.path.basename(filename))[0]
+     img = cv2.imread(filename)
+     print(filename)
+     count = count + 1
+     if count == 4:
+         shutil.copyfile('images/' + basename_without_ext + '.txt', 'valid/' + basename_without_ext + '.txt')
+         cv2.imwrite('valid/' + basename_without_ext + '.jpg', img)
+         count = 0
+     else:
+         shutil.copyfile('images/' + basename_without_ext + '.txt', 'train/' + basename_without_ext + '.txt')
+         cv2.imwrite('train/' + basename_without_ext + '.jpg', img)
 count=0
 for filename in glob.glob('images/*.jpeg'):
     basename_without_ext = os.path.splitext(os.path.basename(filename))[0]
