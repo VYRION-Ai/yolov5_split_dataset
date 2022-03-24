cf08554a4e705acbde94db29c13782d7e5e5df5a
totoadel <mh_882005@yahoo.com>
2022-03-24 10:09:10 +0200

    Update split.py

--- split.py
+++ split.py
@@ -1,47 +1,24 @@
-import cv2
 import glob
 from pathlib import Path
 import os.path
 import shutil
 
-Path("train").mkdir(parents=True, exist_ok=True)
-Path("valid").mkdir(parents=True, exist_ok=True)
+Path("train/images").mkdir(parents=True, exist_ok=True)
+Path("valid/images").mkdir(parents=True, exist_ok=True)
+Path("train/labels").mkdir(parents=True, exist_ok=True)
+Path("valid/labels").mkdir(parents=True, exist_ok=True)
 count = 0
-for filename in glob.glob('images/*.jpg'):
-     basename_without_ext = os.path.splitext(os.path.basename(filename))[0]
-     img = cv2.imread(filename)
-     print(filename)
-     count = count + 1
-     if count == 4:
-         shutil.copyfile('images/' + basename_without_ext + '.txt', 'valid/' + basename_without_ext + '.txt')
-         cv2.imwrite('valid/' + basename_without_ext + '.jpg', img)
-         count = 0
-     else:
-         shutil.copyfile('images/' + basename_without_ext + '.txt', 'train/' + basename_without_ext + '.txt')
-         cv2.imwrite('train/' + basename_without_ext + '.jpg', img)
-count=0
-for filename in glob.glob('images/*.jpeg'):
+for filename in glob.glob('*.jpg'):
     basename_without_ext = os.path.splitext(os.path.basename(filename))[0]
-    img = cv2.imread(filename)
     print(filename)
     count = count + 1
     if count == 4:
-        shutil.copyfile('images/' + basename_without_ext + '.txt', 'valid/' + basename_without_ext + '.txt')
-        cv2.imwrite('valid/' + basename_without_ext + '.jpeg', img)
+        shutil.copyfile(basename_without_ext + '.jpg', 'valid/images/' + basename_without_ext + '.jpg')
+        shutil.copyfile(basename_without_ext + '.txt', 'valid/labels/' + basename_without_ext + '.txt')
+
         count = 0
     else:
-        shutil.copyfile('images/' + basename_without_ext + '.txt', 'train/' + basename_without_ext + '.txt')
-        cv2.imwrite('train/' + basename_without_ext + '.jpeg', img)
-count=0
-for filename in glob.glob('images/*.png'):
-    basename_without_ext = os.path.splitext(os.path.basename(filename))[0]
-    img = cv2.imread(filename)
-    print(filename)
-    count = count + 1
-    if count == 4:
-        shutil.copyfile('images/' + basename_without_ext + '.txt', 'valid/' + basename_without_ext + '.txt')
-        cv2.imwrite('valid/' + basename_without_ext + '.png', img)
-        count = 0
-    else:
-        shutil.copyfile('images/' + basename_without_ext + '.txt', 'train/' + basename_without_ext + '.txt')
-        cv2.imwrite('train/' + basename_without_ext + '.png', img)
+        shutil.copyfile(basename_without_ext + '.jpg', 'train/images/' + basename_without_ext + '.jpg')
+        shutil.copyfile(basename_without_ext + '.txt', 'train/labels/' + basename_without_ext + '.txt')
+
+
